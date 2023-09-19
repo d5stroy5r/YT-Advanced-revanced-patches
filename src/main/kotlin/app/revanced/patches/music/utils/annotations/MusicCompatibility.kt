6212eada5d1812f96d8b1e201815df18b99a4ede
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-19 16:10:51 +0700

    Update supported version

--- src/main/kotlin/app/revanced/patches/music/utils/annotations/MusicCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/annotations/MusicCompatibility.kt
@@ -9,6 +9,9 @@ import app.revanced.patcher.annotation.Package
             "com.google.android.apps.youtube.music",
             arrayOf(
                 "6.15.52",
+                "6.16.52",
+                "6.17.52",
+                "6.18.52",
                 "6.19.51"
             )
         )
