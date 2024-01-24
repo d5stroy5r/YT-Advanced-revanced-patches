75efd8402b03261df9a76d46c0b75493a9ec0de9
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-01-24 22:34:40 +0700

    fix: Done !!!

--- src/main/kotlin/app/revanced/patches/youtube/player/suggestedvideooverlay/SuggestedVideoOverlayPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/player/suggestedvideooverlay/SuggestedVideoOverlayPatch.kt
@@ -82,16 +82,16 @@ object SuggestedVideoOverlayPatch : BytecodePatch(
 
         TouchAreaOnClickListenerFingerprint.result?.let {
             it.mutableMethod.apply {
-                val index = it.scanResult.patternScanResult!!.startIndex + 1
+                val targetindex = it.scanResult.patternScanResult!!.startIndex + 1
                 val targetReference =
                     getInstruction<ReferenceInstruction>(targetIndex).reference.toString()
-                val targetRegister = getInstruction<TwoRegisterInstruction>(index).registerA
+                val targetRegister = getInstruction<Instruction35c>(targetindex).registerC
                 
                 if (targetReference != "Landroid/view/View;->setOnClickListener(Landroid/view/View\$OnClickListener;)V")
                     throw PivotBarSetTextFingerprint.exception
 
                 addInstruction(
-                    index + 1,
+                    targetindex + 1,
                     "invoke-static {v$targetRegister}, $PLAYER->hideSuggestedVideoOverlay(Landroid/view/View;)V"
                 )
             }
