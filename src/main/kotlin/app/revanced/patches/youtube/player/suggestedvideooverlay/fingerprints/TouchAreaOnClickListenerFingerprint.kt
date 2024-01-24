59767f056e50877eee334bb7383c58caf70908b9
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-01-24 18:12:07 +0700

    Create TouchAreaOnClickListenerFingerprint.kt

---
+++ src/main/kotlin/app/revanced/patches/youtube/player/suggestedvideooverlay/fingerprints/TouchAreaOnClickListenerFingerprint.kt
@@ -0,0 +1,26 @@
+package app.revanced.patches.youtube.player.suggestedvideooverlay.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.MethodFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+
+internal object TouchAreaOnClickListenerFingerprint : MethodFingerprint(
+    returnType = "V",
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    parameters = listOf("Ljava/lang/Object"),
+    opcodes = listOf(
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.IF_EQZ,
+        Opcode.CONST_4,
+        Opcode.GOTO,
+        Opcode.CONST_4,
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_STATIC,
+        Opcode.RETURN_OBJECT
+    ),
+    customFingerprint = { methodDef, classDef ->
+        methodDef.name == "b" && classDef.methods.count() == 3
+    }
+)
